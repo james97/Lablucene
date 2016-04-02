2a6401743ae623e0505ad49a43b34517759e3a20
james97 <warrior97@gmail.com>
2016-04-02 18:24:36 -0400

    add an option for apply topic modeling on the whole collection

---
+++ lucene2.4/src/luceneExtend/org/dutir/lucene/TopicModeling.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package org.dutir.lucene;
+
+/**
+ * @author James Miao
+ * The 
+ *
+ */
+public class TopicModeling {
+
+    /**
+     * 
+     */
+    public TopicModeling() {
+        // TODO Auto-generated constructor stub
+    }
+    
+    public void get_LDA_Topics(){
+        
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
